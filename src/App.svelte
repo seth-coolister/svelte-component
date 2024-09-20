622ab159af314b0ca9e53dbf741b5b024e046960
Seth Callister <seth.callister3@gmail.com>
2024-09-19 19:33:19 -0600

    created svelte component
    Taken from REPL to visual studio code

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Button from "./Button.svelte";
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css"/>
+</svelte:head>
+
+<main class="flex justify-center">
+
+  <Button/>
+  <Button withIcon={true} iconPosition="left"/>
+  <Button withIcon={true} iconPosition="right"/>
+  <Button withIcon={true}/>
+	
+</main>
+
---
+++ src/Button.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let withIcon = false;
+	export let iconPosition = "left";
+</script>
+
+<button class="bg-blue-500 text-white w-60 h-12 rounded-full relative ml-4 mt-6 flex justify-center items-center gap-2 hover:bg-purple-500">
+  {#if withIcon && iconPosition === "left"}
+    <div class="w-6 h-6 border border-s-white rounded-full"></div>
+  {/if}
+  <span>Button</span>
+  {#if withIcon && iconPosition === "right"}
+    <div class="w-6 h-6 border border-s-white rounded-full"></div>
+  {/if}
+</button>
